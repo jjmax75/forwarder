bfce5d8f40ca49e4ae940269e3630e78caceb3c8
John Behan <behanj@gmail.com>
2020-10-29 09:56:55 +0000

    setup http server with root route

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('intergalactic');
+});
+
+app.listen(1337, () => console.log('server is listening on http://localhost:1337'));
